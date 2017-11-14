0cfda8cc6060c43ec15fe45db9f74dca0cddcd44
Naotaka Kinoshita <naotakakinoshita@gmail.com>
2017-11-14 10:47:53 -0500

    fixed bug

--- app.py
+++ app.py
@@ -28,7 +28,7 @@ def nasa():
     uResp = urllib2.urlopen("https://api.nasa.gov/planetary/apod?api_key=B8khA5bhcMEjHK9mx680bMIPeksDnaT77CFbZ4S1")
     jason = uResp.read()
     d = json.loads(jason)
-    return render_template("template.html", title = d["title"], author = d["copyright"], date = d["date"], image = d["url"], caption = d["explanation"])
+    return render_template("template.html", title = d["title"], date = d["date"], image = d["url"], caption = d["explanation"])
 
 if __name__ == '__main__':
     app.debug = True
